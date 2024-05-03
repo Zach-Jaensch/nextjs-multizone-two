efa211da01c9f10ac962dbda2234f36574624e3e
Zach Jaensch <zach.jaensch@safetyculture.io>
2024-05-03 21:06:59 +1000

    Update page title

--- app/other/page.tsx
+++ app/other/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main>
-      <h1>Internal page for app 2</h1>
+      <h1>Home page for app 2</h1>
       <Link href="/">Home page for app 1</Link>
       <Link href="/internal">Internal page for app 1</Link>
       <Link href="/other">Home page for app 2</Link>
